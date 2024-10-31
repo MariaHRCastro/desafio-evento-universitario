17526d54809c829565f74ffe4f307c868adb2aee
MariaHRCastro <mariahribeiro@protonmail.com>
2024-10-30 22:05:19 -0300

    Criação das entidades e configuração banco h2

---
+++ src/main/java/com/maria/eventoUniversitario/EventoUniversitarioApplication.java
@@ -0,0 +1,13 @@
+package com.maria.eventoUniversitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoUniversitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoUniversitarioApplication.class, args);
+	}
+
+}
