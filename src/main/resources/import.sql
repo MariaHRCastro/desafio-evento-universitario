a3758e4ce463446320024391c8798a859753bc40
MariaHRCastro <mariahribeiro@protonmail.com>
2024-11-02 22:29:14 -0300

    Ajuste no seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,9 +4,9 @@ INSERT INTO tb_categoria(descricao) VALUES ('Oficina');
 INSERT INTO tb_atividade(categoria_id,nome,descricao,preco) VALUES (1,'Curso de HTML', 'Aprenda HTML de forma prática',80.0);
 INSERT INTO tb_atividade(categoria_id,nome,descricao,preco) VALUES (2,'Oficina de GitHub', 'Controle versões de seus projetos',50.0);
 
-INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (1,TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z' );
-INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z' );
-INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z' );
+INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (1,'2017-09-25 08:00:00','2017-09-25 11:00:00' );
+INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,'2017-09-25 14:00:00','2017-09-25 18:00:00' );
+INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,'2017-09-26 08:00:00','2017-09-26 11:00:00' );
 
 
 INSERT INTO tb_participante(nome,email) VALUES ('Jose Silva', 'jose@gmail.com');
